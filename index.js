d43243a0a84b24c30313eca32456839a011fb2b2
Thomas <65050898+tvonment@users.noreply.github.com>
2022-08-11 17:34:17 +0000

    add twin gedöns

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080;
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 const config = require("./config");
@@ -12,11 +13,12 @@ const client = new CosmosClient({ endpoint, key });
 const database = client.database(databaseId);
 
 const IotClient = require('azure-iothub').Client;
+const Registry = require('azure-iothub').Registry;
 
 const iotConnectionString = process.env.IOTHUB_CONNECTION_STRING;
 if (!iotConnectionString) {
     console.log('Please set the IOTHUB_CONNECTION_STRING environment variable.');
-    //process.exit(-1);
+    process.exit(-1);
 }
 
 const dbConnectionString = process.env.DB_KEY;
@@ -31,13 +33,13 @@ app.use('/favicon.ico', express.static('favicon.ico'));
 
 app.use(express.urlencoded());
 
-
-
 app.get('/', async (req, res) => {
+
     let language = req.query.language;
     let deviceId = req.query.deviceId;
     let deviceLocation = req.query.deviceLocation;
     let verificationCode = req.query.verificationCode;
+
 
     if (!deviceId) {
         res.render("error", { title: "Error", message: "invalid Device ID" });
@@ -46,6 +48,10 @@ app.get('/', async (req, res) => {
     } else if (!language) {
         res.render("home", { title: "Home" });
     } else {
+        let registry = Registry.fromConnectionString(iotConnectionString);
+        let twin = getDeviceTwin(registry, deviceId);
+        // TODO: twin code Validation
+        setNewVerificationCode(registry, twin);
         let languageData = await getLanguageData(req.query.language);
         const locationExists = (e) => e.id === deviceLocation;
         if (!languageData.mzrlocations.items.some(locationExists)) {
@@ -71,20 +77,30 @@ app.listen(port, () => {
     console.log(`This app is listening at http://localhost:${port}`)
 })
 
-function setUpIoT(targetDevice) {
-    const methodParams = {
-        methodName: 'onQrAcknowledged',
-        payload: { "complete": true },
-        responseTimeoutInSeconds: 15 // set response timeout as 15 seconds
-    };
-    console.log(targetDevice, iotConnectionString);
-    let iotClient = IotClient.fromConnectionString(iotConnectionString);
-    iotClient.invokeDeviceMethod(targetDevice, methodParams, (err, result) => {
+function getDeviceTwin(registry, deviceId) {
+    registry.getTwin(deviceId, (err, twin) => {
         if (err) {
-            console.error('Failed to invoke method \'' + methodParams.methodName + '\': ' + err.message);
+            console.error(err.message);
         } else {
-            console.log(methodParams.methodName + ' on ' + targetDevice + ':');
-            console.log(JSON.stringify(result, null, 2));
+            console.log(JSON.stringify(twin));
+            return twin;
+        }
+    });
+}
+
+function setNewVerificationCode(registry, twin) {
+    twin.update(twinPatch, (err, twin) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log(JSON.stringify(twin));
+            registry.updateTwin(twin.deviceId, { properties: { desired: { verificationCode: uuidv4() } } }, twin.etag, (err, twin) => {
+                if (err) {
+                    console.error(err.message);
+                } else {
+                    console.log(JSON.stringify(twin));
+                }
+            });
         }
     });
 }
